8bccd6bab8845acef1957726d8a40cad79b40273
Innokenty Beloborodov <kennix88@icloud.com>
2022-01-31 19:15:47 +0800

    connecting ngRx + Creating a register action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Store} from "@ngrx/store";
+import {registerAction} from "../store/actions/register.action";
 
 @Component({
   selector: 'app-register',
@@ -10,10 +12,9 @@ export class RegisterComponent implements OnInit{
   form!: FormGroup
 
   constructor(
-    private fb: FormBuilder
-  ) {
-
-  }
+    private fb: FormBuilder,
+    private store: Store
+  ) {}
 
   ngOnInit() {
     this._createForm()
@@ -25,11 +26,12 @@ export class RegisterComponent implements OnInit{
       email: ['', Validators.required],
       password: ['', Validators.required]
     })
-}
+  }
 
-onSubmit(): void {
-  console.log(this.form.valid)
-  console.log(this.form.value)
-}
+  onSubmit(): void {
+    console.log(this.form.valid)
+    console.log(this.form.value)
+    this.store.dispatch(registerAction(this.form.value))
+  }
 
 }
